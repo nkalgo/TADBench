6ada517a5169f44cc006bd31be981dd85f8f65b0
nkalgo <2120230754@mail.nankai.edu.cn>
2025-04-07 10:21:56 +0800

    Update base.py

--- sdk/base.py
+++ sdk/base.py
@@ -1,13 +1,10 @@
 from abc import ABC, abstractmethod
-import pandas as pd
 
 
 class TADTemplate(ABC):
-    def __init__(self, dataset_name=None, train_path=None, test_path=None, output_path=None):
+    def __init__(self, dataset_name=None, data_path=None):
         self.dataset_name = dataset_name
-        self.train_path = train_path
-        self.test_path = test_path
-        self.output_path = output_path
+        self.data_path = data_path
 
     @abstractmethod
     def preprocess_data(self):
@@ -20,7 +17,3 @@ class TADTemplate(ABC):
     @abstractmethod
     def test(self):
         pass
-
-
-
-
